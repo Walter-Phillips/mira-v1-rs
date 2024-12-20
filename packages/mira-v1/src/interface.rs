acfd13a6a568d4c6e1f6475ee63cd506fb074d40
Walter-Phillips <phillips.walter.n@gmail.com>
2024-12-19 18:09:26 -0800

    reading binary from local project root

--- packages/mira-v1/src/interface.rs
+++ packages/mira-v1/src/interface.rs
@@ -1,17 +1,30 @@
 use fuels::prelude::*;
 
-pub const AMM_CONTRACT_BINARY_PATH: &str =
-    "sway-abis/mira_amm_contract/release/mira_amm_contract.bin";
-pub const ADD_LIQUIDITY_SCRIPT_BINARY_PATH: &str =
-    "sway-abis/add_liquidity_script/release/add_liquidity_script.bin";
-pub const CREATE_POOL_AND_ADD_LIQUIDITY_SCRIPT_BINARY_PATH: &str =
-    "sway-abis/create_pool_and_add_liquidity_script/release/create_pool_and_add_liquidity_script.bin";
-pub const REMOVE_LIQUIDITY_SCRIPT_BINARY_PATH: &str =
-    "sway-abis/remove_liquidity_script/release/remove_liquidity_script.bin";
-pub const SWAP_EXACT_INPUT_SCRIPT_BINARY_PATH: &str =
-    "sway-abis/swap_exact_input_script/release/swap_exact_input_script.bin";
-pub const SWAP_EXACT_OUTPUT_SCRIPT_BINARY_PATH: &str =
-    "sway-abis/swap_exact_output_script/release/swap_exact_output_script.bin";
+pub const AMM_CONTRACT_BINARY_PATH: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "sway-abis/mira_amm_contract/release/mira_amm_contract.bin"
+);
+
+pub const ADD_LIQUIDITY_SCRIPT_BINARY_PATH: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "sway-abis/add_liquidity_script/release/add_liquidity_script.bin"
+);
+pub const REMOVE_LIQUIDITY_SCRIPT_BINARY_PATH: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "sway-abis/remove_liquidity_script/release/remove_liquidity_script.bin"
+);
+pub const SWAP_EXACT_INPUT_SCRIPT_BINARY_PATH: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "sway-abis/swap_exact_input_script/release/swap_exact_input_script.bin"
+);
+pub const SWAP_EXACT_OUTPUT_SCRIPT_BINARY_PATH: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "sway-abis/swap_exact_output_script/release/swap_exact_output_script.bin"
+);
+pub const CREATE_POOL_AND_ADD_LIQUIDITY_SCRIPT_BINARY_PATH: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "sway-abis/create_pool_and_add_liquidity_script/release/create_pool_and_add_liquidity_script.bin"
+);
 
 abigen!(
     Script(
